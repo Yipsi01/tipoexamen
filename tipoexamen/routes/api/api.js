bfbb0f51b630a447156aa8df522526728508cac8
Yipsi <yipsiavila437@gmail.com>
2019-10-15 00:17:53 -0600

    Estructura de Api incompleta

---
+++ tipoexamen/routes/api/api.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.router{};
+ 
+var PersonaRouters = Require {'./Personas'};
+
+router.user ('/PersonasRoutes');
+
+module.exports = router;
---
+++ tipoexamen/routes/api/personas.js
@@ -0,0 +1,45 @@
+
+var express = require ('express');
+var rauter = express.Router();
+var uuid = require('uuid/V4');    //generar identificador unico
+
+var PersonaCollection = [];
+var PersonaStruct = {
+
+    id :0,
+    nombre: 'Yipsi',
+    correo: 'ymoncadaavila@gmail.com',
+    telefono: '98998999',
+    nota: '0000',
+}
+
+
+PersonaCollection.push(
+
+);
+
+router.get('/:id',(req,res,next)=>{
+    res.status(403).json({msg:"not implemented"});
+});          //GET ONE BY ID
+
+
+router.get ('/',(req,res,next)=>{
+    res.status(200).json(PersonasColleccion);
+});              //METODO GET
+
+router.post('/', (req,res,next)=>{
+    res.status(403).json({msg:"not implemented"});
+});    //METODO POST
+
+rauter.put('/:id', (req,res,next)=>{
+    res.status(403)({mgs:"not implemented"});
+});        //METODO PUT
+
+router.delete('/:id',(req,next,res)=>{
+    res.status(403)({mgs:"not implemented"});
+});                         //METODO DELETE
+
+module.exports = router;
+
+
+
