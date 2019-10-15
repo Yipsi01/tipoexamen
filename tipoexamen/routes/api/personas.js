5043b4dbaf2434a74af546cf822fe83ba625a371
Yipsi <yipsiavila437@gmail.com>
2019-10-15 01:09:19 -0600

    Final de Tipo Examen

--- tipoexamen/routes/api/personas.js
+++ tipoexamen/routes/api/personas.js
@@ -19,7 +19,17 @@ PersonaCollection.push(
 );
 
 router.get('/:id',(req,res,next)=>{
-    res.status(403).json({msg:"not implemented"});
+    if(!req.params.id)return next();
+    var id=req.params.id;
+    var persona=PersonaCollecion.filter((e,i)=>{return (e.id===id);
+    });   //FILTER
+    if(persona.lenght > 0){
+        res.status(200).json(persona[0]);
+    }else {
+
+        res.status(404).json({});
+    }
+ 
 });          //GET ONE BY ID
 
 
@@ -40,11 +50,33 @@ router.post('/', (req,res,next)=>{
       //METODO POST
 
 rauter.put('/:id', (req,res,next)=>{
-    res.status(403)({mgs:"not implemented"});
+    var id = req.params.id;
+    var modifiedPersona = {};
+    var originalPersona = {};
+
+    PersonaCollecion = PersonaCollecion.map( (e, i)=>{
+        if (e.id ===id){originalPersona=Object.assign({}, e, req.body);
+    }
+    return e;
+    });
+    
+    res.status(200).json({0:originalPersona, m:modifiedPersona});
 });        //METODO PUT
 
 router.delete('/:id',(req,next,res)=>{
-    res.status(403)({mgs:"not implemented"});
+
+    var id = req.params.id;
+    var deletedPersona = {};
+    PersonaCollecion = PersonaCollecion.filter((e,i)=>{
+        if(e.id ===id)
+        deletedPersona = Object.assign({},e);
+        return false;
+    }
+    return true;
+
+    });   //FILTER
+
+    res.status(200).json({d:deletedPersona, c:PersonaCollecion});
 });                         //METODO DELETE
 
 module.exports = router;
